f1051c5a83608130a34092087b0be30ed7a7ca35
George <georgecbrainard4@gmail.com>
2022-09-11 15:02:50 -0400

    add shuffleDeck function

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ const reset = document.getElementById('reset-button')
 // Event listeners
 document.getElementById('btn').addEventListener('click', handleClick)
 
-reset.addEventListener("click", init)
+reset.addEventListener("click", shuffleReset)
 
 // Functions
 
@@ -24,7 +24,9 @@ init()
 
 // Initialize deck 1 with array of 52 cards 
 function init() {
-  deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+    deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+    messageEl.textContent = 'Game of War'
+    
 }
 
 // Function to handle a button click:
@@ -70,13 +72,46 @@ function render(cardPicked) {
   
 }
 	
-    function shuffleCards(){
-
+    function shuffleReset(){
+        deck1.sort((a, b)=>{
+                if (Math.random()> 0.5){    
+                    return -1 
+                } else {
+                    return 1   
+                }
+            })
+            console.log(deck1)
+                
+            
     }
+        
+//     for (let i = 0; i < 52; i++) {
+//         let tempCard = deck1[i];
+//         let randomIndex = Math.floor(Math.random() * 52);
+//         deck1[i] = deck1[randomIndex];
+//         deck1[randomIndex] = tempCard;
+//     }
+// }
+//     let testDeck = createDeck();
+//     shuffleDeck(testDeck);
+//     console.log(testDeck);
+    
+    
+    
+    
+    // }
 
-    function getLoser(){
-        // if player || cpuPlayer = noCards
-    }
+    // function renderWin(div) {
+    //     messageEl.className = 'winner'
+    //     div.className = 'winner'
+    //     deck1El.appendChild(div)
+
+    //     if (deck1.length === 1){
+    //         messageEl.textContent = 
+    //     }
+
+        
+    // }
 
 
 
@@ -92,8 +127,8 @@ function render(cardPicked) {
 // 5. defined the required constants. cards should have suit and rank. deck will be collection of cards. shuffle and dealt one at a time
 // 6. handle a player clicking a card with a handle click function
 // 7. build the "getWinner" function. player runs out of cards
-// 8. create Reset functionality
-// 9. shuffle deck function
+// 8. dealCards function
+// 9. shuffle deck function.create Reset functionality
 // 10. eight is the highest number mode
 // 11. alternative keyboard function
 // 12. hand will also be collection of cards 
