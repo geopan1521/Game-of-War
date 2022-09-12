1bff99c1107615b72cdd593671c8bc1bc40c4715
George <georgecbrainard4@gmail.com>
2022-09-12 15:09:38 -0400

    add deck2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Declare deck variables
 let deck1 = []
-let computerDeck = []
+let deck2 = []
 let cardToRemove
 
 
@@ -14,7 +14,7 @@ const messageEl = document.getElementById('message')
 console.log(messageEl)
 
 const reset = document.getElementById('reset-button')
-const computerDeckElement = document.querySelector(".computer-deck")
+
 // Event listeners
 document.getElementById('btn').addEventListener('click', handleClick)
 
@@ -35,45 +35,38 @@ function init() {
 
 // Function to handle a button click:
 function handleClick() {
-  if (deck1.length > 0) {
-    // Randomly select number from total cards remaining
+    if (deck1.length > 0) {
     let randIdx = Math.floor(Math.random() * deck1.length)
-    // Assign card with the random index to a variable
     let cardPicked = deck1.splice(randIdx, 1)[0]
-    // Add card picked to deck 2
     deck2.push(cardPicked)
-    // Pass card picked to render function to display
     render(cardPicked)
-  }
+    }
 }
 
 
 // Function to render deck state
 function render(cardPicked) {
-  console.log(cardPicked)
-  // Remove outline class when first card is picked
-  if (deck2.length === 1) {
+    console.log(cardPicked)
+    if (deck2.length === 1) {
     deck2El.classList.remove('outline')
-  }
-  // Removes previous picked card from deck 2 class list
-  if (deck2.length > 1) {
+}
+    
+    if (deck2.length > 1) {
     deck2El.classList.remove(cardToRemove)
-  }
+}
 
-  cardToRemove = cardPicked
-  // Add current card picked to deck 2 element
-  deck2El.classList.add(cardPicked)
-  // Adjust shadow when deck gets above/below halfway full
-  if (deck2.length === 26) {
+    cardToRemove = cardPicked
+    deck2El.classList.add(cardPicked)
+    if (deck2.length === 26) {
     deck2El.classList.add('shadow')
     deck1El.classList.remove('shadow')
-  }
-  // Remove card back color and add outline when last card is picked
-  if (deck1.length === 0) {
+}
+
+    if (deck1.length === 0) {
     deck1El.classList.add('outline')
     deck1El.classList.remove('back-blue')
-  }
-  
+}
+
 }
 	
     function shuffleReset(){
